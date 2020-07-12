7b8c36903306898eae6df303030805a31c46ce5a
vien-tran <vien-tran@cybozu.vn>
2020-07-12 09:36:44 +0700

    add new blank line at two data scope

--- acceptance/src/schedule/test-specs/added-new-appoiment/added-new-appointment.data.js
+++ acceptance/src/schedule/test-specs/added-new-appoiment/added-new-appointment.data.js
@@ -7,20 +7,24 @@ const currentDate = new DateTimeGenerator().adjustDateTime({day: 0});
 let currentHours = new Date().getHours();
 const appointment = new RegularAppointment();
 appointment.subject = `Regular Appointment title-${(new Date()).toString()}`;
+
 appointment.startMonth = currentDate.getMonth();
 appointment.startDay = currentDate.getDay();
 appointment.startYear = currentDate.getYear();
 appointment.startHour = (currentHours + 1).toString();
 appointment.startMinute = '00';
+
 appointment.endMonth = currentDate.getMonth();
 appointment.endDay = currentDate.getDay();
 appointment.endYear = currentDate.getYear();
 appointment.endHour = (currentHours + 2).toString();
 appointment.endMinute = '00';
+
 appointment.attendees = [];//["user1","user2"];
 appointment.facilities = [];
 appointment.attachments = [];
 appointment.visibility = 'Public';
+
 appointment.notes = 'Note';
 
 const testData = {appointmentInfo: appointment, signInUser: signInUser};
