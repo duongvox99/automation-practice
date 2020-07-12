47465119ef064d1fb1e0d71f36c219e12350b8ca
vien-tran <vien-tran@cybozu.vn>
2020-07-12 10:11:24 +0700

    add new blank line at two scopes

--- acceptance/src/schedule/test-specs/added-new-appoiment/added-new-appointment.data.js
+++ acceptance/src/schedule/test-specs/added-new-appoiment/added-new-appointment.data.js
@@ -6,13 +6,14 @@ const signInUser = {username: "user1", password: "user1"};
 const currentDate = new DateTimeGenerator().adjustDateTime({day: 0});
 let currentHours = new Date().getHours();
 const appointment = new RegularAppointment();
-appointment.subject = `Regular Appointment title-${(new Date()).toString()}`;
+
+appointment.subject = `Cybozu automation testing workshop ${(new Date()).toString()}`;
 
 appointment.startMonth = currentDate.getMonth();
 appointment.startDay = currentDate.getDay();
 appointment.startYear = currentDate.getYear();
-appointment.startHour = (currentHours + 1).toString();
-appointment.startMinute = '00';
+appointment.startHour = (currentHours + 1).toString(); // e.g: 17
+appointment.startMinute = '00'; // to emulate the end-user behavior
 
 appointment.endMonth = currentDate.getMonth();
 appointment.endDay = currentDate.getDay();
@@ -20,12 +21,12 @@ appointment.endYear = currentDate.getYear();
 appointment.endHour = (currentHours + 2).toString();
 appointment.endMinute = '00';
 
-appointment.attendees = [];//["user1","user2"];
+appointment.attendees = []; // ["user1","user2"];
 appointment.facilities = [];
 appointment.attachments = [];
 appointment.visibility = 'Public';
 
-appointment.notes = 'Note';
+appointment.notes = 'Please join the AWS on time';
 
 const testData = {appointmentInfo: appointment, signInUser: signInUser};
 
